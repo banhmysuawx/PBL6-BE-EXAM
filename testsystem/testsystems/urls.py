46e9fd143c4a55cf8cc6fcce41846cb099d75e83
Ngo Nguyen Hoang Dung <nnhoangdungdh@gmail.com>
2022-12-26 01:38:40 +0700

    feature get api for list and retrive question

--- testsystem/testsystems/urls.py
+++ testsystem/testsystems/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import TestView, DoTestView, CategoryView, ResultView
+from .views import TestView, DoTestView, CategoryView, ResultView, QuestionView
 urlpatterns = [
     path('test', TestView.as_view({'get':'list', 'post':'create'})),
     path('test/<int:pk>', TestView.as_view({'get':'retrieve', 'delete':'destroy'})),
     path('test/<int:pk>/doing', DoTestView.as_view({'post':'doing_test'})),
     path('test/category', CategoryView.as_view()),
+    path('test/question', QuestionView.as_view({'get':'list'})),
+    path('test/question/<int:pk>', QuestionView.as_view({'get':'retrieve'})),
     path('results', ResultView.as_view({'get':'list'})),
     # path("result/<int:user_id>/", ResultView.as_view({'get':'retrieve', 'delete':'destroy'})),
     path("results/<int:user_id>/jobs/<int:job_id>/", ResultView.as_view({'get':'retrieve', 'delete':'destroy'})),
